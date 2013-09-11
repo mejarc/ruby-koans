93b5392dc6166e83cfe3dfb2cbf67cbc533ce4a5
Melanie Archer <job@twobanjos.com>
2013-09-11 08:42:21 -0700

    Daily progress.

---
+++ koan-test.rb
@@ -0,0 +1,15 @@
+# Encoding: utf-8
+# first_thing, = ["foo", "bar"]  # first_thing is a String, "foo"
+# puts first_thing
+
+# first_thing = ["foo", "bar"]  # first_thing is the Array's values
+# puts first_thing
+
+# first_thing, * = ["foo", "bar", "baz"]
+# puts first_thing
+#     long_string = <<EOS
+# It was the best of times,
+# It was the worst of times.
+# EOS
+
+# puts Math.sqrt(5)
